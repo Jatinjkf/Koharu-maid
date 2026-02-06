725236ce48aa8c388965449aa0e6094f6edb87c9
Jatinjkf <72139428+Jatinjkf@users.noreply.github.com>
2026-02-06 12:29:17 +0530

    Update move.js

--- src/commands/move.js
+++ src/commands/move.js
@@ -43,11 +43,12 @@ module.exports = {
         }
     },
     async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
         const itemIdOrName = interaction.options.getString('item');
         const freqName = interaction.options.getString('to');
         const guildId = interaction.guild.id;
 
-        if (freqName === "none") return interaction.reply({ content: "Please configure rhythms in the Web Admin Panel first.", ephemeral: true });
+        if (freqName === "none") return interaction.editReply({ content: "Please configure rhythms in the Web Admin Panel first." });
 
         // Find Item
         let item = await Item.findById(itemIdOrName);
@@ -59,11 +60,11 @@ module.exports = {
              }
         }
 
-        if (!item) return interaction.reply({ content: 'I could not find that item in your active list.', ephemeral: true });
+        if (!item) return interaction.editReply({ content: 'I could not find that item in your active list.' });
 
         // Find Frequency
         const frequency = await Frequency.findOne({ guildId, name: freqName });
-        if (!frequency) return interaction.reply({ content: 'I do not know that rhythm. Please select one from the list.', ephemeral: true });
+        if (!frequency) return interaction.editReply({ content: 'I do not know that rhythm. Please select one from the list.' });
 
         // Update
         item.frequencyName = frequency.name;
@@ -74,6 +75,6 @@ module.exports = {
         await item.save();
         await updateDashboard(interaction.client, guildId, interaction.user.id);
 
-        return interaction.reply({ content: `As you wish. I have moved "**${item.name}**" to the **${freqName}** schedule.`, ephemeral: true });
+        return interaction.editReply({ content: `As you wish. I have moved "**${item.name}**" to the **${freqName}** schedule.` });
     }
 };
